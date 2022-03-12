3151fb3b4ed9ef86fced54bd27c7c8955e39a720
Matheus Cavalcante <jmatheusoc269@gmail.com>
2022-03-12 10:58:29 -0300

    registrando e mostrando quantidade de contatos

--- src/com/poo/Agenda.java
+++ src/com/poo/Agenda.java
@@ -1,4 +1,31 @@
 package com.poo;
 
+import java.util.ArrayList;
+
 public class Agenda {
+  private  ArrayList<Contato> registros = new ArrayList<Contato>();
+
+  //private Contato contato;
+
+
+    public ArrayList<Contato> getRegistros() {
+        return registros;
+    }
+
+    public void setRegistros(ArrayList<Contato> registros) {
+        this.registros = registros;
+    }
+
+    public void registrarContato(Contato contato) {
+        registros.add(contato);
+
+    }
+
+    public int  quantContatos() {
+        int quantidade = registros.size();
+
+
+    return quantidade;
+    }
+
 }
--- src/com/poo/Contato.java
+++ src/com/poo/Contato.java
@@ -1,6 +1,38 @@
 package com.poo;
 
 public class Contato {
-    String nome;
-    int numero;
+    private String nome;
+    private int numero;
+    private int id;
+
+
+    public Contato(String nome, int numero, int id) {
+        this.nome = nome;
+        this.numero = numero;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
 }
